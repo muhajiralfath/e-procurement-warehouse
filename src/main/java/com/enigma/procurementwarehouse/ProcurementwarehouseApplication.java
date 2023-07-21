20e4ae5ceec89a67b1baafa1f7b0f6b3f86ca045
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-07-21 22:58:59 +0700

    initial project warehouse procurement

---
+++ src/main/java/com/enigma/procurementwarehouse/ProcurementwarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.procurementwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementwarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementwarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/procurementwarehouse/ProcurementwarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.procurementwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementwarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
