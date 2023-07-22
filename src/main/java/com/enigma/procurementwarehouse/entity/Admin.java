9566579c36609923d849c20f7ad4150f232e421c
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-07-22 12:44:14 +0700

    [FEATURE] User Credential and Uder Details

---
+++ src/main/java/com/enigma/procurementwarehouse/entity/Admin.java
@@ -0,0 +1,31 @@
+package com.enigma.procurementwarehouse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "m_admin")
+public class Admin {
+
+    @Id
+    @GenericGenerator(strategy = "uuid2", name = "system-uuid")
+    @GeneratedValue(generator = "system-uuid")
+    private String id;
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "user_credential_id")
+    private UserCredential userCredential;
+}
